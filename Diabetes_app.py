303cf0103c89c432c4e1c55a082faae9cdb0a9c3
Samira-Hassann <152163512+Samira-Hassann@users.noreply.github.com>
2025-01-20 20:44:19 +0200

    Diabetes_app.py

--- Diabetes_app.py
+++ Diabetes_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pickle
+import os
+if not os.path.exists("lr.pkl"):
+    st.error("The model file 'lr.pkl' is missing.")
+else:
+    lr_model = pickle.load(open("lr.pkl", "rb"))
 lr_model = pickle.load(open(r"lr.pkl","rb"))
 # App title
 st.title("Diabetes detection app")
