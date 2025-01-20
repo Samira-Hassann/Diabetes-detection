520d5e89870267ddccf9c227c92ee7047aca858c
Samira-Hassann <152163512+Samira-Hassann@users.noreply.github.com>
2025-01-20 20:46:01 +0200

    Diabetes_app.py

--- Diabetes_app.py
+++ Diabetes_app.py
@@ -5,30 +5,3 @@ if not os.path.exists("lr.pkl"):
     st.error("The model file 'lr.pkl' is missing.")
 else:
     lr_model = pickle.load(open("lr.pkl", "rb"))
-lr_model = pickle.load(open(r"lr.pkl","rb"))
-# App title
-st.title("Diabetes detection app")
-
-# Create input fields for numerical features
-st.subheader("Fill these data:")
-
-# Input fields for each feature
-pregnancies = st.number_input("Pregnancies")
-glucose = st.number_input("Glucose Level")
-blood_pressure = st.number_input("Blood Pressure")
-skin_thickness = st.number_input("Skin Thickness")
-insulin = st.number_input("Insulin Level")
-bmi = st.number_input("BMI")
-diabetes_pedigree = st.number_input("Diabetes Pedigree Function")
-age = st.number_input("Age")
-
-
-
-if st.button("Submit"):
-    data = [[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree, age]]
-    pred = lr_model.predict(data)
-    
-    if pred[0] == 1:
-        st.success("You have Diabetes")
-    else:
-        st.success("You don't have Diabetes")
